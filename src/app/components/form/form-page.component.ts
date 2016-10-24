3349ef6e5d5185b4ee14dc27a179ded470214805
Bastiaan van den Berg <b.a.vandenberg@gmail.com>
2016-10-24 21:04:21 +0200

    Update

--- src/app/components/form/form-page.component.ts
+++ src/app/components/form/form-page.component.ts
@@ -1,10 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
     selector: 'form-page',
     templateUrl: 'form-page.html'
 })
 
-export class FormPageComponent {
+export class FormPageComponent implements OnInit {
 
+    form: FormGroup;
+
+    lastSubmittedName: string;
+    lastSubmittedAmount: number;
+
+    ngOnInit() {
+        this.form = new FormGroup({
+            name: new FormControl('', Validators.required),
+            amount: new FormControl('', Validators.required)
+        });
+    }
+
+    onFormSubmit() {
+        this.lastSubmittedName = this.form.controls['name'].value;
+        this.lastSubmittedAmount = this.form.controls['amount'].value;
+    }
 }
